52ff0cce7ae8484f10937dfd6fb0bcf1c5350160
Nandha-Dev <nandhakumar89056@gmail.com>
2026-01-06 20:52:36 +0530

    04: Organizations

--- apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ apps/web/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
-import {SignUp} from "@clerk/nextjs";
+import { SignUpView } from '@/modules/auth/ui/views/sign-up-view';
 
 const Page = () => {
-    return (
-        <SignUp />
-    );
+    return <SignUpView />;
 };
-export default Page
+export default Page;
