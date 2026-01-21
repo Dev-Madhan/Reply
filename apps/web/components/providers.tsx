7b7a96a4b1a0f225bab54df2cec6824e04cae5cd
Dev-Madhan <maddyclickz24@gmail.com>
2026-01-21 21:55:02 +0530

    11: Widget Screen Router Completed.

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -1,22 +1,29 @@
 "use client"
 
-import { ReactNode } from "react"
-import { ConvexReactClient } from "convex/react"
-import { ConvexProviderWithClerk } from "convex/react-clerk"
+import {ReactNode} from "react"
+import {ConvexReactClient} from "convex/react"
+import {ConvexProviderWithClerk} from "convex/react-clerk"
 import {useAuth} from "@clerk/nextjs";
+import {Provider} from "jotai";
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL
 
 if (!convexUrl) {
-  throw new Error("NEXT_PUBLIC_CONVEX_URL is missing")
+    throw new Error("NEXT_PUBLIC_CONVEX_URL is missing")
 }
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
-  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
+    throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
 }
 
 const convex = new ConvexReactClient(convexUrl)
 
-export function Providers({ children }: { children: ReactNode }) {
-  return <ConvexProviderWithClerk client={convex} useAuth={useAuth}>{children}</ConvexProviderWithClerk>
+export function Providers({children}: { children: ReactNode }) {
+    return (
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+            <Provider>
+                {children}
+            </Provider>
+        </ConvexProviderWithClerk>
+    )
 }
---
+++ apps/widget/modules/widget/types.ts
@@ -0,0 +1,1 @@
+export type WidgetScreen = {typeof WIDGET_SCREENS} [number];
